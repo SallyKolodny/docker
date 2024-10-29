37a20dd9049427b2c4f2f8c92c9d65cb21ea263a
Sally Kolodny <sallykolodny@protonmail.com>
2024-10-28 22:12:29 -0400

    Updating code...

---
+++ bin/add-node-to-cluster.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+kubeadm join 192.168.0.86:6443 --token bxitin.w5azonmcewwndctz \
+        --discovery-token-ca-cert-hash sha256:1444d0fce34045cd110d8f02a8a7f6cc22c068e2e90130e45b7adb439a76e169
+
